83de0640406050c3684fd5bf0773e1edc1d3b9ae
Mathias Nord <mathias.erik.nord@gmail.com>
2025-01-12 00:12:11 +0100

    Started work on CLI.

--- src/main/java/userInteraction/MainMenu.java
+++ src/main/java/userInteraction/MainMenu.java
@@ -1,5 +1,56 @@
 package userInteraction;
 
-public class MainMenu {
+import utils.PrintHandler;
 
+public class MainMenu {
+  private StringMenu stringMenu;
+  private PrintHandler printHandler;
+
+  public MainMenu() {
+    this.stringMenu = new StringMenu();
+    this.printHandler = new PrintHandler();
+  }
+
+  private enum MainCommands {
+    ENCRYPT(1),
+    DECRYPT(2),
+    EXIT(0);
+    private final int value;
+
+    MainCommands(int value) {
+      this.value = value;
+    }
+
+    private static MainCommands fromValue(int value) {
+      for (MainCommands command : MainCommands.values()) {
+        if (command.value == value) {
+          return command;
+        }
+      }
+      throw new IllegalArgumentException("The command value is invalid" + value);
+    }
+  }
+
+
+  public void mainMenu() {
+    MainCommands command = null;
+
+    do {
+      printHandler.printString(stringMenu.getMainMenu());
+      int commandValue;
+
+      try {
+        commandValue = inputHandler.intReader("Enter command: ");
+        command = MainCommands.fromValue(commandValue);
+        commandHandler(command);
+      }
+
+    } while (command != MainCommands.EXIT);
+  }
+
+  private void commandHandler(MainCommands command) {
+    switch (command) {
+      case ENCRYPT ->
+    }
+  }
 }
---
+++ src/main/java/userInteraction/StringMenu.java
@@ -0,0 +1,20 @@
+package userInteraction;
+
+public class StringMenu {
+  public StringMenu() {
+
+  }
+
+  public String getMainMenu() {
+    return """
+        ######## Welcome to the Image Encrypter ########
+        
+        [1] - Encrypt Image
+        [2] - Decrypt Image
+        [0] - Exit
+        
+        ################################################
+        """;
+  }
+
+}
